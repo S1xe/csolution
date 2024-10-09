8acba1d4b6dd6f3afdb2d911adf7fb5d1bbf5053
S1xe <914588599@qq.com>
2024-10-09 14:26:08 +0800

    Update test.py

--- pycode/test.py
+++ pycode/test.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 
 if __name__ == '__main__':
-    pprint({"data" : {}})
-    print({"data" : "value"})
+    pprint({"data": {}})
+    print({"data": "value"})
     print("Hello World")
 
